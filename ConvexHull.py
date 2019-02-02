d47d7be720741747c437196c49adc6c668ef0ff8
Bruno Maciel <brunomaciel.xc4@gmail.com>
2019-02-02 21:07:04 -0200

    Add method to plot the hull

--- ConvexHull.py
+++ ConvexHull.py
@@ -1,8 +1,9 @@
 from math import atan2, pi
+import matplotlib.pyplot as plt
 
 
 class ConvexHull(object):
-    __matrix = []
+    __all_points = []
     __hull = []
 
     def __init__(self, x, y):
@@ -20,7 +21,7 @@ class ConvexHull(object):
         max_y = [self.__x[0], self.__y[0], 0]
 
         for i in range(len(self.__y)):
-            self.__matrix.append([self.__x[i], self.__y[i]])
+            self.__all_points.append([self.__x[i], self.__y[i]])
 
             if self.__y[i] < min_y[1]:
                 min_y = [self.__x[i], self.__y[i], i]
@@ -35,38 +36,38 @@ class ConvexHull(object):
         # Garante que 1 pontos não sirva como maior/menor valor para o eixo x e y ao mesmo tempo
         if max_x == min_y:
             max_x = [0, 0, 0]
-            for i in range(len(self.__matrix)):
-                if self.__matrix[i][0] > max_x[0] and self.__matrix[i] != [min_y[0], min_y[1]]:
-                    max_x = [self.__matrix[i][0], self.__matrix[i][1], i]
+            for i in range(len(self.__all_points)):
+                if self.__all_points[i][0] > max_x[0] and self.__all_points[i] != [min_y[0], min_y[1]]:
+                    max_x = [self.__all_points[i][0], self.__all_points[i][1], i]
 
         if max_x == max_y:
             max_y = min_y
-            for i in range(len(self.__matrix)):
-                if self.__matrix[i][1] > max_y[1] and self.__matrix[i] != [max_x[0], max_x[1]]:
-                    max_y = [self.__matrix[i][0], self.__matrix[i][1], i]
+            for i in range(len(self.__all_points)):
+                if self.__all_points[i][1] > max_y[1] and self.__all_points[i] != [max_x[0], max_x[1]]:
+                    max_y = [self.__all_points[i][0], self.__all_points[i][1], i]
 
         if min_x == max_y:
             min_x = max_x
-            for i in range(len(self.__matrix)):
-                if self.__matrix[i][0] < min_x[0] and self.__matrix[i] != [max_y[0], max_y[1]]:
-                    min_x = [self.__matrix[i][0], self.__matrix[i][1], i]
+            for i in range(len(self.__all_points)):
+                if self.__all_points[i][0] < min_x[0] and self.__all_points[i] != [max_y[0], max_y[1]]:
+                    min_x = [self.__all_points[i][0], self.__all_points[i][1], i]
 
         if min_x == min_y:
             min_x = max_x
-            for i in range(len(self.__matrix)):
-                if self.__matrix[i][0] < min_x[0] and self.__matrix[i] != [min_y[0], min_y[1]]:
-                    min_x = [self.__matrix[i][0], self.__matrix[i][1], i]
+            for i in range(len(self.__all_points)):
+                if self.__all_points[i][0] < min_x[0] and self.__all_points[i] != [min_y[0], min_y[1]]:
+                    min_x = [self.__all_points[i][0], self.__all_points[i][1], i]
 
         # Reorganiza a matriz de pontos colocando os limites nas primeiras posicoes
         # ficando as primeiras posicoes ocupadas com a ordem: min_y, max_x, max_y, min_x
-        self.__matrix.remove([min_x[0], min_x[1]])
-        self.__matrix.remove([max_y[0], max_y[1]])
-        self.__matrix.remove([max_x[0], max_x[1]])
-        self.__matrix.remove([min_y[0], min_y[1]])
-        self.__matrix.insert(0, [min_x[0], min_x[1]])
-        self.__matrix.insert(0, [max_y[0], max_y[1]])
-        self.__matrix.insert(0, [max_x[0], max_x[1]])
-        self.__matrix.insert(0, [min_y[0], min_y[1]])
+        self.__all_points.remove([min_x[0], min_x[1]])
+        self.__all_points.remove([max_y[0], max_y[1]])
+        self.__all_points.remove([max_x[0], max_x[1]])
+        self.__all_points.remove([min_y[0], min_y[1]])
+        self.__all_points.insert(0, [min_x[0], min_x[1]])
+        self.__all_points.insert(0, [max_y[0], max_y[1]])
+        self.__all_points.insert(0, [max_x[0], max_x[1]])
+        self.__all_points.insert(0, [min_y[0], min_y[1]])
 
         del (min_y[2], max_x[2], max_y[2], min_x[2])
 
@@ -83,19 +84,19 @@ class ConvexHull(object):
         # pois o calculo do angulo varia de acordo com o checkpoint atual
         angle = 7  # em radianos
 
-        for i in range(checkpoint, len(self.__matrix)):
-            resp = self.__compute_angle(current_pt, self.__matrix[i])
+        for i in range(checkpoint, len(self.__all_points)):
+            resp = self.__compute_angle(current_pt, self.__all_points[i])
             if checkpoint == 1 and resp < angle:
                 angle = resp
-                new_pt = [self.__matrix[i][0], self.__matrix[i][1], i]
+                new_pt = [self.__all_points[i][0], self.__all_points[i][1], i]
             elif checkpoint == 2 and (pi / 2) < resp < angle:
                 angle = resp
-                new_pt = [self.__matrix[i][0], self.__matrix[i][1], i]
+                new_pt = [self.__all_points[i][0], self.__all_points[i][1], i]
             elif checkpoint == 3 and pi < resp < angle:
                 angle_to_1st_pt = self.__compute_angle(current_pt, self.__hull[0])
                 if resp < angle_to_1st_pt:
                     angle = resp
-                    new_pt = [self.__matrix[i][0], self.__matrix[i][1], i]
+                    new_pt = [self.__all_points[i][0], self.__all_points[i][1], i]
                 else:
                     angle = angle_to_1st_pt
                     new_pt = [self.__hull[0][0], self.__hull[0][1], i]
@@ -103,11 +104,11 @@ class ConvexHull(object):
                 angle_to_1st_pt = self.__compute_angle(current_pt, self.__hull[0])
                 if resp < angle_to_1st_pt:
                     angle = resp
-                    new_pt = [self.__matrix[i][0], self.__matrix[i][1], i]
+                    new_pt = [self.__all_points[i][0], self.__all_points[i][1], i]
                 else:
                     angle = angle_to_1st_pt
                     new_pt = [self.__hull[0][0], self.__hull[0][1], i]
-            elif (checkpoint == 4 or checkpoint == 3) and i == len(self.__matrix) - 1 and angle == 7:
+            elif (checkpoint == 4 or checkpoint == 3) and i == len(self.__all_points) - 1 and angle == 7:
                 new_pt = [self.__hull[0][0], self.__hull[0][1], i]
 
         # Verifica se alcancou um novo checkpoint
@@ -116,8 +117,8 @@ class ConvexHull(object):
                 checkpoint = i + 1
 
         # remove o ponto encontrado da matrix pois ele não é mais candidato para ser o proximo ponto do hull
-        if [new_pt[0], new_pt[1]] != self.__matrix[checkpoint - 1]:
-            del (self.__matrix[new_pt[2]])
+        if [new_pt[0], new_pt[1]] != self.__all_points[checkpoint - 1]:
+            del (self.__all_points[new_pt[2]])
         del (new_pt[2])
         self.__hull.append(new_pt)
 
@@ -140,14 +141,37 @@ class ConvexHull(object):
 
         return angle
 
+    def plot_hull(self):
+        # Funcao para plotar o grafico do hull e dos demais pontos
 
-from random import uniform
+        # Pontos que compoem o hull
+        x = []
+        y = []
+        for i in range(len(self.__hull)):
+            x.append(self.__hull[i][0])
+            y.append(self.__hull[i][1])
 
-dados_x = []
-dados_y = []
-for _ in range(18):
-    dados_x.append(round(uniform(0, 10), 2))
-    dados_y.append(round(uniform(0, 10), 2))
+        # Pontos que compoem os limites inferiores e superiores dos eixos x e y
+        x1 = []
+        y1 = []
+        for i in range(4):
+            x1.append(self.__all_points[i][0])
+            y1.append(self.__all_points[i][1])
 
-print(f'dados_x = {dados_x}\ndados_y = {dados_y}')
-c_hull = ConvexHull(dados_x, dados_y)
+        # Demais pontos
+        x2 = []
+        y2 = []
+        for i in range(4, len(self.__all_points)):
+            x2.append(self.__all_points[i][0])
+            y2.append(self.__all_points[i][1])
+
+        plt.plot(x, y)
+        plt.scatter(x, y, s=20)
+        plt.scatter(x1, y1, color="y", marker="o")
+        plt.scatter(x2, y2, color="r", marker="+")
+        plt.show()
+        plt.close()
+
+    @property
+    def hull(self):
+        return self.__hull
